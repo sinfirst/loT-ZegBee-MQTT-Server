5c8e379485f6a45de65bb12c716e50effa8e76ff
sinfirst <nikitakoz2006@gmail.com>
2025-11-21 16:04:26 +0300

    add some things

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,72 @@
 package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM, syscall.SIGINT, syscall.SIGQUIT)
+	defer cancel()
+
+	conf, err := config.NewConfig()
+	if err != nil {
+		logger.Fatalw("can't init config", err)
+	}
+
+	logger := logging.NewLogger(conf.LogLvl)
+	db := postgresbd.NewPGDB(conf, logger)
+
+	a := app.NewHTTPServer(logger)
+	router := router.NewRouter(a)
+
+	if conf.DatabaseDsn != "" {
+		err := postgresbd.InitMigrations(conf, logger)
+		if err != nil {
+			logger.Fatalw("can't init migrations", err)
+		}
+	}
+	server := &http.Server{Addr: conf.ServerAddress, Handler: router}
+	if !conf.HTTPSEnable {
+		go func() {
+			logger.Infow("Starting http server", "addr", conf.ServerAddress)
+			if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+				logger.Fatalw("create server error: ", err)
+			}
+		}()
+	} else {
+		go func() {
+			logger.Infow("Starting https server")
+			err := http.ListenAndServeTLS(":8443", certFile, keyFile, nil)
+			if err != nil {
+				logger.Fatal("error while start server: ", err)
+			}
+		}()
+	}
+
+	listen, err := net.Listen("tcp", ":3200")
+	if err != nil {
+		log.Fatal(err)
+	}
+	s := grpc.NewServer(grpc.UnaryInterceptor(logging.LoggingUnaryInterceptor(logger)))
+	pb.RegisterURLCutterServer(s, grpcserver.NewURLCutterServer(logger, handlers))
+	fmt.Println("Сервер gRPC начал работу")
+	if err := s.Serve(listen); err != nil {
+		log.Fatal(err)
+	}
+
+	<-ctx.Done()
+	if err := server.Shutdown(context.Background()); err != nil {
+		logger.Errorw("Server shutdown error", err)
+	}
+	workers.StopWorker()
+	close(deleteCh)
+}
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,85 @@
+// Package config пакет с инициализацией конфига
 package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gopkg.in/yaml.v3"
+)
+
+// Config основная структура конфигурации
+type Config struct {
+	App      AppConfig      `yaml:"app"`
+	Log      LogConfig      `yaml:"log"`
+	MQTT     MQTTConfig     `yaml:"mqtt"`
+	HTTP     HTTPConfig     `yaml:"http"`
+	DataBase DataBaseConfig `yaml:"database"`
+}
+
+type AppConfig struct {
+	Name    string `yaml:"name"`
+	Version string `yaml:"version"`
+}
+
+type LogConfig struct {
+	Level  string `yaml:"level"`
+	Output string `yaml:"output,omitempty"`
+}
+
+type MQTTConfig struct {
+	Broker           string `yaml:"broker"`
+	ClientID         string `yaml:"client_id"`
+	Username         string `yaml:"username,omitempty"`
+	Password         string `yaml:"password,omitempty"`
+	New_divice_topic string `yaml:"new_divice_topic"`
+	QoS              int    `yaml:"qos"`
+	CleanSession     bool   `yaml:"clean_session"`
+	KeepAlive        int    `yaml:"keep_alive"`
+	ConnectTimeout   int    `yaml:"connect_timeout"`
+}
+
+type HTTPConfig struct {
+	Host    string `yaml:"host"`
+	Port    int    `yaml:"port"`
+	Address string
+}
+
+type DataBaseConfig struct {
+	Host        string `yaml:"host"`
+	Port        int    `yaml:"port"`
+	Name        string `yaml:"name"`
+	User        string `yaml:"user"`
+	Password    string `yaml:"password"`
+	DataBaseDSN string
+}
+
+func LoadConfig(configPath string) (*Config, error) {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+
+	config := &Config{}
+	file, err := os.Open(configPath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	decoder := yaml.NewDecoder(file)
+	if err := decoder.Decode(config); err != nil {
+		return nil, err
+	}
+
+	overrideFromEnv(config)
+	config.HTTP.Address = fmt.Sprintf("%s:%d", config.HTTP.Host, config.HTTP.Port)
+	config.DataBase.DataBaseDSN = fmt.Sprintf("%s://%s:%s@%s:%d", "postgres", config.DataBase.User, config.DataBase.Password, config.DataBase.Host, config.DataBase.Port)
+
+	return config, nil
+}
+
+func overrideFromEnv(config *Config) {
+	// надо описать для докера
+}
